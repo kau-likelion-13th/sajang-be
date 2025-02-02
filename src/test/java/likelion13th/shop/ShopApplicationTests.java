efa5c0f9de1a36d6faedb110ad40ef9131ca96a5
yaaan7 <wangpajeon@gmail.com>
2025-02-02 23:25:01 +0900

    프로젝트 생성

---
+++ src/test/java/likelion13th/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion13th.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
