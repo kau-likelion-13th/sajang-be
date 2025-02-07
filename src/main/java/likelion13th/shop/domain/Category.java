e9b689973bba41d32a16950b8e71fb1ed4991863
yaaan7 <wangpajeon@gmail.com>
2025-02-07 13:34:06 +0900

    feat: Category 기본 필드 추가 (#6)

--- src/main/java/likelion13th/shop/domain/Category.java
+++ src/main/java/likelion13th/shop/domain/Category.java
@@ -1,4 +1,20 @@
 package likelion13th.shop.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+//
 public class Category {
+    @Id
+    @GeneratedValue
+    @Column(name="category_id")
+    private Long id;
+
+    @Column(name="category_name")
+    private String name;
 }
