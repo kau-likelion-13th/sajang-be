61f967e484cc8703410d643dbfdf55f95140a0a5
yaaan7 <wangpajeon@gmail.com>
2025-02-07 13:38:52 +0900

    feat: Item 기본 필드 추가 (#6)

--- src/main/java/likelion13th/shop/domain/Item.java
+++ src/main/java/likelion13th/shop/domain/Item.java
@@ -1,4 +1,28 @@
 package likelion13th.shop.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDate;
+@Entity
+@Getter
+@Setter
 public class Item {
+    @Id
+    @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String item_name;
+    private int price;
+    private String thumbnail_img;
+    private String brand;
+
+    @CreatedDate
+    private LocalDate created_date;
 }
