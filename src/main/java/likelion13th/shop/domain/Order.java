97aa442b99710352fe992be3230104b6dea4b482
yaaan7 <wangpajeon@gmail.com>
2025-02-07 15:07:37 +0900

    Order, Item, User 양방향 메서드 추가 (#6)

--- src/main/java/likelion13th/shop/domain/Order.java
+++ src/main/java/likelion13th/shop/domain/Order.java
@@ -43,5 +43,18 @@ public class Order {
     @JoinColumn(name = "user_id")
     private User user;
 
+    //양방향 편의 메서드
+    public void setUser(User user) {
+        this.user=user;
+        user.getOrders().add(this); // 반대쪽 객체에도 연관관계를 설정
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+        if (!item.getOrders().contains(this)) {
+            item.getOrders().add(this);
+        }
+    }
+    
 
 }
