fdf6de1261dea8082c6bd9dccf2e068eec3049ea
yaaan7 <wangpajeon@gmail.com>
2025-02-07 13:25:48 +0900

    feat: Address 기본 필드 추가

--- src/main/java/likelion13th/shop/domain/Address.java
+++ src/main/java/likelion13th/shop/domain/Address.java
@@ -1,4 +1,22 @@
 package likelion13th.shop.domain;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String zipcode;
+    @Lob
+    @Column(columnDefinition = "TEXT")
+    //TEXT 타입을 java 엔티티에서 매핑
+    private String address;
+    @Lob
+    @Column(columnDefinition = "TEXT")
+    private String address_detail;
+
+
+
 }
