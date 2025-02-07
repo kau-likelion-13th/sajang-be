8cd88f724ea8f4dbc61cb996ad49aec3baef9114
yaaan7 <wangpajeon@gmail.com>
2025-02-07 13:57:35 +0900

    feat: User 기본 필드 추가 (#6)

--- src/main/java/likelion13th/shop/domain/User.java
+++ src/main/java/likelion13th/shop/domain/User.java
@@ -1,5 +1,38 @@
 package likelion13th.shop.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="user_id")
+    private Long id;
+
+    private String username;
+
+    private String provider_id;
+    private Boolean deleteable; //베릴님꺼에는 db에 비트라 되어있어요
+    private Long phone_numer;
+
+    private int mileage;
+    private int recent_total;
+
+    //erd에는 있길래..
+    //private String grade;
+    //private String profile_img_path;
+    //private String password;
+
+    @CreatedDate
+    private LocalDate sub_date;
+    
+
 
 }
