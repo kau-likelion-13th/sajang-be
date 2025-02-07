af46e9f82b1ac93a98419ea17c94cbcd966aa847
yaaan7 <wangpajeon@gmail.com>
2025-02-07 14:13:43 +0900

    feat: Order 기본 필드 추가, Orderstatus 연관관계 설정 (#6)

--- src/main/java/likelion13th/shop/domain/Order.java
+++ src/main/java/likelion13th/shop/domain/Order.java
@@ -1,4 +1,27 @@
 package likelion13th.shop.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
 public class Order {
+    @Id
+    @GeneratedValue
+    @Column(name="order_id")
+    private Long id;
+
+    private int quantity;
+    private int totalPrice;
+    private int finalPrice;
+    private LocalDateTime created_date;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+    
+
 }
