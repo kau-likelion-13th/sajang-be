b0dba6e023f23e6c34df3c754a39203841a0ae26
yaaan7 <wangpajeon@gmail.com>
2025-02-08 21:43:47 +0900

    ItemRepository 생성 (#6)

---
+++ src/main/java/likelion13th/shop/repository/ItemRepository.java
@@ -0,0 +1,11 @@
+package likelion13th.shop.repository;
+
+
+import likelion13th.shop.domain.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long>{
+    //save, findById, findAll 같은 기본 기능 자동으로 제공됨
+}
---
+++ src/main/java/likelion13th/shop/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package likelion13th.shop.repository;
+
+public class UserRepository {
+}
