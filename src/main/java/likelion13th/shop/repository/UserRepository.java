4632bdfc723c5be651392b8fd5dc45561f6826cf
yaaan7 <wangpajeon@gmail.com>
2025-02-08 21:55:40 +0900

    Order, User Repository 생성 (#6)

--- src/main/java/likelion13th/shop/repository/UserRepository.java
+++ src/main/java/likelion13th/shop/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package likelion13th.shop.repository;
 
-public class UserRepository {
+import likelion13th.shop.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository {
+    //save, findById, findAll은 자동생성
+    List<User> findByName(String name);
 }
