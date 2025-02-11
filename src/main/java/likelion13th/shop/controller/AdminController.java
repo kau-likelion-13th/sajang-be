71bc0ffa4356b7953dd141d6b485863245695cf6
yaaan7 <wangpajeon@gmail.com>
2025-02-11 16:59:52 +0900

    refactor: AdminController Deleate 수정

--- src/main/java/likelion13th/shop/controller/AdminController.java
+++ src/main/java/likelion13th/shop/controller/AdminController.java
@@ -2,6 +2,7 @@ package likelion13th.shop.controller;
 
 import likelion13th.shop.DTO.ItemCreateRequest;
 import likelion13th.shop.DTO.ItemUpdateRequest;
+import likelion13th.shop.domain.Category;
 import likelion13th.shop.domain.Item;
 import likelion13th.shop.repository.CategoryRepository;
 import likelion13th.shop.repository.ItemRepository;
@@ -25,7 +26,7 @@ public class AdminController {
         Item newItem = itemService.createItem(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(newItem);
     }
-    
+
     //상품 수정
     @PutMapping("/{itemId}")
     public ResponseEntity<Item> updateItem(
@@ -44,6 +45,10 @@ public class AdminController {
     //테스트 데이터 전체 삭제 API
     @DeleteMapping("/reset")
     public ResponseEntity<String> resetDatabase() {
+        //중간테이블 삭제
+        for (Category category : categoryRepository.findAll()) {
+            category.getItems().clear(); // 카테고리와 아이템의 관계 해제
+        }
         itemRepository.deleteAll();   // 상품 전체 삭제
         categoryRepository.deleteAll();  // 카테고리 전체 삭제
         return ResponseEntity.ok("테스트 데이터가 모두 삭제되었습니다.");
