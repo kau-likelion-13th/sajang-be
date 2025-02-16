3a5aa7bc5fe6b52ebe826f34c229b4b88ee4830f
yaaan7 <wangpajeon@gmail.com>
2025-02-16 10:48:07 +0900

    refactor: CategoryController 수정 (#10)

--- src/main/java/likelion13th/shop/controller/CategoryController.java
+++ src/main/java/likelion13th/shop/controller/CategoryController.java
@@ -3,40 +3,46 @@ package likelion13th.shop.controller;
 import likelion13th.shop.DTO.CategoryCreateRequest;
 import likelion13th.shop.DTO.CategoryResponseDto;
 import likelion13th.shop.DTO.ItemResponseDto;
+import likelion13th.shop.global.api.ApiResponse;
+import likelion13th.shop.global.api.ErrorCode;
+import likelion13th.shop.global.api.SuccessCode;
 import likelion13th.shop.service.CategoryService;
-import likelion13th.shop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/category")
+@RequestMapping("/categories")
 @RequiredArgsConstructor
 public class CategoryController {
     private final CategoryService categoryService;
 
     //카테고리 추가
     @PostMapping("/new")
-    public  ResponseEntity<CategoryResponseDto> createCategory(@RequestBody CategoryCreateRequest request){
+    public ApiResponse<CategoryResponseDto> createCategory(@RequestBody CategoryCreateRequest request){
         CategoryResponseDto newCategory = categoryService.createCategory(request);
-        URI location = URI.create("/category/" + newCategory.getId());
-        return ResponseEntity.created(location).body(newCategory);
+        return  ApiResponse.onSuccess(SuccessCode.CATEGORY_CREATE_SUCCESS, newCategory);
     }
 
     //카테고리 전체 조회
     @GetMapping
-    public ResponseEntity<List<CategoryResponseDto>> getAllCategories() {
+    public ApiResponse<?> getAllCategories() {
         List<CategoryResponseDto> categories = categoryService.getAllCategories();
-        return ResponseEntity.ok(categories);
+        if (categories.isEmpty()) {
+            return ApiResponse.onFailure(ErrorCode.CATEGORY_NOT_FOUND, "조회된 카테고리가 없습니다.");
+        }
+        return ApiResponse.onSuccess(SuccessCode.CATEGORY_GET_SUCCESS, categories);
     }
 
     //특정 카테고리에 속한 상품 목록 조회
     @GetMapping("/{categoryId}/items")
-    public ResponseEntity<List<ItemResponseDto>> getItemsByCategory(@PathVariable Long categoryId) {
+    public ApiResponse<?> getItemsByCategory(@PathVariable Long categoryId) {
         List<ItemResponseDto> items = categoryService.getItemsByCategory(categoryId);
-        return ResponseEntity.ok(items);
+        if (items.isEmpty()) {
+            return ApiResponse.onFailure(ErrorCode.CATEGORY_NOT_FOUND,  "해당 카테고리에 등록된 상품이 없습니다.");
+        }
+        return ApiResponse.onSuccess(SuccessCode.CATEGORY_ITEMS_GET_SUCCESS, items);
     }
 }
--- src/main/java/likelion13th/shop/global/api/ErrorCode.java
+++ src/main/java/likelion13th/shop/global/api/ErrorCode.java
@@ -23,6 +23,11 @@ public enum ErrorCode implements BaseCode { // 실패
     TOKEN_INVALID(HttpStatus.FORBIDDEN, "JWT_4032", "유효하지 않은 토큰입니다."),
     TOKEN_NO_AUTH(HttpStatus.FORBIDDEN, "JWT_4033", "권한 정보가 없는 토큰입니다."),
     TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "JWT_4011", "토큰 유효기간이 만료되었습니다."),
+
+    // Category
+    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "CATEGORY_4041", "해당 카테고리를 찾을 수 없습니다."),
+    CATEGORY_ITEMS_NOT_FOUND(HttpStatus.NOT_FOUND, "CATEGORY_4042", "해당 카테고리에 상품이 없습니다.");
+
     ;
 
     private final HttpStatus httpStatus;
