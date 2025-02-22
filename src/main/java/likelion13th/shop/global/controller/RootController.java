669999ace5f67bb17b25b4491b9bd3d5a245b023
MinJunKKang <kmg4613@naver.com>
2025-02-22 20:47:55 +0900

    RestController로 변경 및 API 응답형식

--- src/main/java/likelion13th/shop/global/controller/RootController.java
+++ src/main/java/likelion13th/shop/global/controller/RootController.java
@@ -1,16 +1,20 @@
 package likelion13th.shop.global.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@Tag(name = "health", description = "health check 관련 api 입니다.")
+@RestController
 public class RootController {
-
+    @Operation(summary = "cicd 확인", description = "cicd가 정상적으로 작동하고 있는지 확인하는 메서드입니다.")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON_200", description = "Success"),
+    })
     @GetMapping("/health")
-    public ResponseEntity<String> healthCheck() {
-        return new ResponseEntity<>("OK", HttpStatus.OK);
+    public String healthCheck() {
+        return "OK";
     }
-
 }
