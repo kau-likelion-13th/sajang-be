42070b349045500d9ee4cc9062e1f4aecca42e58
MinJunKKang <kmg4613@naver.com>
2025-03-23 17:09:51 +0900

    localhost:3000번 추가

--- src/main/java/likelion13th/shop/global/config/SecurityConfig.java
+++ src/main/java/likelion13th/shop/global/config/SecurityConfig.java
@@ -83,7 +83,8 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList(
                 "http://localhost:8080",
-                "http://SAJANG-dev-env-2.eba-3ycixkjh.ap-northeast-2.elasticbeanstalk.com"
+                "http://SAJANG-dev-env-2.eba-3ycixkjh.ap-northeast-2.elasticbeanstalk.com",
+                "http://localhost:3000"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
